a127f8a359bed48a0e3fe76aca065d8b51d940f5
Amr Loulah <amrloulah2021@gmail.com>
2026-01-31 02:41:08 +0200

    feat: Add Azure constants for endpoint, API key, model ID, and API version.

---
+++ lib/core/constants/azure_constants.dart
@@ -0,0 +1,10 @@
+abstract class AzureConstants {
+  // NOTE: Keys are hidden for security reasons.
+
+  static const String endpoint = 'YOUR_AZURE_ENDPOINT_HERE';
+
+  static const String apiKey = 'YOUR_AZURE_API_KEY_HERE';
+
+  static const String modelId = 'prebuilt-invoice';
+  static const String apiVersion = '2024-11-30';
+}
